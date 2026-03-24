262e9f8ac1155bf8c81fdbaea42da14d9d492300
manvityagi29 <manvityagi29july@gmail.com>
2026-03-24 21:42:08 +0530

    Updated cpp-core

--- cpp-core/main.cpp
+++ cpp-core/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>   // for file reading
 #include <string>
 
 using namespace std;
@@ -13,38 +12,12 @@ RLEResult RunLengthEncode(string input);
 int HuffmanEncoding(string input);
 int main()
 {
-    int choice;
-    string filename;
     string input;
-    cout << "Choose Sample File:\n";
-    cout << "1. Repetitive Data\n";
-    cout << "2. Random Data\n";
-    cout << "3. Skewed Data\n";
-    cout << "Enter choice: ";
-    cin >> choice;
-
-    if (choice == 1)
-        filename = "../samples/repetitive.txt";
-    else if (choice == 2)
-        filename = "../samples/random.txt";
-    else
-        filename = "../samples/skewed.txt";
-
-    ifstream file(filename);
-
-    if (!file)
-    {
-        cout << "Error: Cannot open file!\n";
-        return 1;
-    }
-
-    file >> input;
-    cout << "\nInput Read From File: " << input << endl;
-
+    cout << "Enter text to analyze: ";
+    cin >> input;
     int fixedBits = CalculateFixedSize(input);
     RLEResult rleResult = RunLengthEncode(input);
     int huffmanBits = HuffmanEncoding(input);
-
     cout << "\n===== Compression Results =====\n";
     cout << "Fixed Encoding Bits : " << fixedBits << endl;
     cout << "RLE Encoded Output  : " << rleResult.encoded << endl;
@@ -53,21 +26,16 @@ int main()
 
     int minBits = fixedBits;
     string bestAlgo = "Fixed Encoding";
-
     if (rleResult.totalBits < minBits)
     {
         minBits = rleResult.totalBits;
         bestAlgo = "Run Length Encoding (RLE)";
     }
-
     if (huffmanBits < minBits)
     {
         minBits = huffmanBits;
         bestAlgo = "Huffman Encoding";
     }
-
     cout << "\n>>> Best Algorithm: " << bestAlgo << endl;
     cout << ">>> Minimum Bits Used: " << minBits << endl;
-
-    return 0;
 }
